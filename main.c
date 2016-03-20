575d1989a31fab71eb4035395e5ad79f455eda1d
Igor Golovin <mr.igor.golovin@gmail.com>
2016-03-20 17:54:37 +0400

    Update main.c

--- main.c
+++ main.c
@@ -16,34 +16,44 @@ int main(int argc, char* argv[])
         return 2;
     }
     
+    int rows = 0;
+    int cols = 0;
+    
+    fscanf(pFile, "%d%d", &rows, &cols);
+    
+    fseek(pFile, 0, SEEK_SET);
+    
     Matrix* matrix = create_matrix_from_file(pFile);
     
-    Matrix* transp = create_matrix(matrix->cols, matrix->rows);
+    fclose(pFile);
+    
+    Matrix* transp = create_matrix(cols, rows);
     
     double val = 0.0;
     int i, j;
-    for (i = 0; i < transp->rows; i++)
+    for (i = 0; i < cols; i++)
     {
-        for (j = 0; j < transp->cols; j++)
+        for (j = 0; j < rows; j++)
         {
             val = get_elem(matrix, j, i);
             set_elem(transp, i, j, val);
         }
     }
     
-    for (i = 0; i < matrix->rows; i++)
+    for (i = 0; i < rows; i++)
     {
-        for (j = 0; j < matrix->cols; j++)
+        for (j = 0; j < cols; j++)
         {
             printf("%lf \t", get_elem(matrix, i, j));
         }
         putchar('\n');
     }
+    
     putchar('\n');
-        
-    for (i = 0; i < transp->rows; i++)
+   
+    for (i = 0; i < cols; i++)
     {
-        for (j = 0; j < transp->cols; j++)
+        for (j = 0; j < rows; j++)
         {
             printf("%lf \t", get_elem(transp, i, j));
         }

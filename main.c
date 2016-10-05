f76c3e2d52cd83ffc7ab961b7c73f8956f722613
zwirec <zwirec@icloud.com>
2016-10-05 21:43:23 +0300

    Реализация файла main.c

--- main.c
+++ main.c
@@ -12,19 +12,22 @@ int main(int argc, char *argv[])
     }
 
     FILE *fileInput = NULL;
-    if (argv[argc-1] == "gen") {
+    
+    /*if (argv[argc-1] == "gen") {
         generate_matrix_to_file(10,10,100);
-    }
+    }*/
+    
     if ((fileInput = fopen(argv[1], "r")) == 0) {
         printf("File not exists!\n");
         return 1;
     }
-    Matrix* man = create_matrix_from_file(fileInput);
-    print_matrix(man);
-    printf("max-norm = %.3lf\nl-norm = %.3lf\ne-norm = %.3lf\n",
-                    max_norm(man), l_norm(man), e_norm(man));
-    man = transpose(man);
-    print_matrix(man);
+    Matrix* mat = create_matrix_from_file(fileInput);
+    //print_matrix(mat);
+    printf("max-norm = %.3lf\n", max_norm(mat));
+    //printf("max-norm = %.3lf\nl-norm = %.3lf\ne-norm = %.3lf\n",
+    //               max_norm(mat), l_norm(mat), e_norm(mat));
+    //mat = transpose(mat);
+    print_matrix(mat);
     free_matrix(man);
     return 0;
 }

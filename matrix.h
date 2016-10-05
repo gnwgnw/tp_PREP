1c130f0ffd569080db6831d957a97f3126913ab5
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-10-05 19:41:00 +0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -2,9 +2,19 @@
 #define _MATRIX_H_
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
 
-typedef struct Matrix {
-    //TODO
+#define MAX_LENGTH 255
+#define ERROR_EXIT 1
+#define SUCCESS_EXIT 0
+
+typedef struct {
+	double** matrix;
+	int rows_N;
+	int cols_M;
 } Matrix;
 
 Matrix* create_matrix_from_file(FILE* file);
@@ -14,5 +24,7 @@ double get_elem(Matrix* matrix, int row, int col);
 void set_elem(Matrix* matrix, int row, int col, double val);
 int get_rows(Matrix* matrix);
 int get_cols(Matrix* matrix);
+void check_file(FILE* file);
+void print_matrix(Matrix* matrix);
 
 #endif //_MATRIX_H_

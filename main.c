98c926c1f92c252cd2ca1b93f1b8f1f99139877a
Drujininii <Drujininii@yandex.ru>
2016-10-05 06:38:05 +0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
 #include "matrix.h"
 
-int main(int argc, char* argv[])
+#define NO_NAME -1
+#define CANT_READ -2
+#define ERROR -1
+
+Matrix *matrix;
+
+int comparator(const void*, const void*);
+int swap(Matrix *, int);
+void transpon(Matrix *);
+void print_matrix(const int, const int, const Matrix *);
+int *consider_struct_element(const Matrix *);
+
+int main(int argc, char *argv[])
 {
-	//TODO
+	FILE *fp;
+	//обработка ошибок
+	if (argc < 2) {
+		printf("нет имени файла с матрцицей\n");
+		return NO_NAME;
+	}
+	if ((fp = fopen(argv[1], "r")) == NULL) {
+			printf("не могу прочесть файл %s\n", argv[1]);
+			return CANT_READ;
+		}
+
+	fp = fopen(argv[1], "r");
+	matrix = create_matrix_from_file(fp);
+	if (matrix == NULL)
+		return ERROR;
+	fclose(fp);
+
+	int l = 0;
+	int row;
+	row = get_rows(matrix);
+	if (row == -1 || row == -2) {
+		printf("ошибка чтения матрицы: %d\n", row);
+		return ERROR;
+	}
+	int col;
+	col = get_cols(matrix);
+	if (col == -1 || col == -2) {
+		printf("ошибка чтения матрицы: %d\n", col);
+		return ERROR;
+	}
+	//транспонирование
+	transpon(matrix);
+
+	//печать транспонированной матрицы
+	print_matrix(col, row, matrix);
+
+	free_matrix(matrix);
 	return 0;
 }
+//транспонирование матрицы
+
+void transpon(Matrix *matrix)
+{
+	//перемена индексов элементов(транспонирование)
+	int i = 0;
+	while((*(matrix + i)).val != '0') {
+		swap(matrix, i);
+		i++;
+	}
+	//подсчет ненулевых элементов и сортировка
+	int *strel;
+	strel = consider_struct_element(matrix);
+	qsort(matrix, *strel, sizeof(*matrix), comparator);
+}
+
+
+int comparator(const void* v1, const void* v2)
+{
+ return ((*((Matrix*)v2)).row < (*((Matrix*)v1)).row);
+}
+
+
+int swap(Matrix *matrix, int i)
+{
+   int temp;
+   temp = (*(matrix+i)).row;
+   (*(matrix+i)).row = (*(matrix+i)).col;
+   (*(matrix+i)).col = temp;
+   return 1;
+}
+
+void print_matrix(const int col, const int row, const Matrix *matrix) {
+	int m = 0;
+	int difind;
+	double dnull = 0;
+	printf("транспонированная матрица: \n");
+	for(int i = 0; i < col; i++){
+		printf("\n");
+		for(int j = 0; j < row; j++){
+			if ((*(matrix + m)).col != j){
+				printf("%6.2f", dnull);
+				continue;
+			}
+			printf("%6.2f", (*(matrix + m)).val);
+			m++;
+		}
+	}
+	printf("\n");
+}
+
+int *consider_struct_element(const Matrix *matrix) {
+	int *b, p;
+	p = 0;
+	b = &p;
+	while((*(matrix + *b)).val != '0') //подсчет количества элементов
+			(*b)++;
+	return b;
+}
+
--- matrix.h
+++ matrix.h
@@ -4,15 +4,18 @@
 #include <stdio.h>
 
 typedef struct Matrix {
-    //TODO
+    int col;
+    int row;
+    double val;
 } Matrix;
 
-Matrix* create_matrix_from_file(FILE* file);
-Matrix* create_matrix(int row, int col);
-void free_matrix(Matrix* matrix);
-double get_elem(Matrix* matrix, int row, int col);
-void set_elem(Matrix* matrix, int row, int col, double val);
-int get_rows(Matrix* matrix);
-int get_cols(Matrix* matrix);
+Matrix* create_matrix_from_file(FILE* file); //чтение из файла
+Matrix* create_matrix(int row, int col); //создание объекта типа матрицы
+void free_matrix(Matrix* matrix); //освобождение памяти из под матрицы
+double get_elem(Matrix* matrix, int row, int col); //достать элемент матрицы
+void set_elem(Matrix* matrix, int row, int col, double val); //переназначить элемент матрицы
+int get_rows(Matrix* matrix); //определить количество строк
+int get_cols(Matrix* matrix); //определить количество столбцов
 
 #endif //_MATRIX_H_
+

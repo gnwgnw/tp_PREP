bfa409e6a99cf09495cfbe9fb8399fe23f3a30bf
MikeGus <vfc17@yandex.ru>
2016-10-06 01:40:15 +0300

    Add files via upload

--- matrix.h
+++ matrix.h
@@ -1,11 +1,21 @@
-#ifndef _MATRIX_H_
-#define _MATRIX_H_
+#ifndef MATRIX_H
+#define MATRIX_H
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Element {
+    int row_ind;
+    int col_ind;
+    double elem;
+} Element;
 
 typedef struct Matrix {
-    //TODO
+    int row;
+    int col;
+    Element* elems;
 } Matrix;
+
 
 Matrix* create_matrix_from_file(FILE* file);
 Matrix* create_matrix(int row, int col);
@@ -15,4 +25,5 @@ void set_elem(Matrix* matrix, int row, int col, double val);
 int get_rows(Matrix* matrix);
 int get_cols(Matrix* matrix);
 
-#endif //_MATRIX_H_
+
+#endif // MATRIX_H
